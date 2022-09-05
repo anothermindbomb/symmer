b617656e88c385e01391bf676ceb643a70a886b4
Steve Flynn <anothermindbomb@gmail.com>
2022-09-05 21:13:50 +0100

    Add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::fs::create_dir_all;
+use indicatif::{ProgressBar, ProgressStyle};
 use std::fs::File;
+use std::fs::{create_dir_all, remove_file};
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::os::windows::fs::symlink_file;
@@ -8,27 +9,52 @@ use std::path::Path;
 fn main() -> std::io::Result<()> {
     // Some code to experiment with creating symlinks.
 
-    // We use a file of legitimate filenames and break them up into path and filename, then use the file type to determine what
-    // sample we're going to symlink to.
-
     let driverfilename = r"C:\Users\Steve\Desktop\Swordfish\swordfishlivedocs.txt";
     let driverhandle = File::open(driverfilename)?;
-    let reader = BufReader::new(driverhandle);
+    let fakereader = BufReader::new(&driverhandle);
+    let mut line_cnt = 0;
+
+    // Determine how many lines we're going to process by simply counting them all. We don't store them, as
+    // there may be many millions - 15 GB text files are not unheard of.
+    for _ in fakereader.lines() {
+        line_cnt += 1;
+    }
+
+    let bar = ProgressBar::new(line_cnt);
+    bar.set_style(
+        ProgressStyle::with_template(
+            "[{elapsed_precise}] {per_sec} {wide_bar} {pos:>7}/{len:7} ETA: {eta} {msg}",
+        )
+        .unwrap(),
+    );
+
+    // We use a file of legitimate filenames and break them up into path and filename, then use the file type to determine what
+    // sample we're going to symlink to.
+    let driverfilename = r"C:\Users\Steve\Desktop\Swordfish\swordfishlivedocs.txt";
+    let driverhandle = File::open(driverfilename)?;
+    let reader = BufReader::new(&driverhandle);
+
     for line in reader.lines() {
         let line = line.unwrap();
         let fullpath = Path::new(&line);
         let basepath = fullpath.parent().unwrap().to_str().unwrap();
-        let _filename = fullpath.file_name().unwrap().to_str().unwrap();
+        // let _filename = fullpath.file_name().unwrap().to_str().unwrap();
         let extension = fullpath.extension().unwrap().to_str().unwrap();
         // dbg!(basepath, filename, extension);
 
         // So we know the path, the filename and the extension we're dealing with.
+
         // Create the path if it doesn't already exist.
         create_dir_all(&basepath)?;
+
+        // Build our target filename up. Raw string to help with the slashes, and we need to own the result.
         let mut targetfile = r#"J:\\SampleDocs\SampleDocument."#.to_owned();
-        targetfile += extension;
-        // dbg!(filename, targetfile);
-        symlink_file(fullpath, targetfile)?;
+        targetfile += extension; // append the correct file extension.
+
+        remove_file(&line).ok(); // remove the file and don't worry it it fails.
+        symlink_file(targetfile, line)?; // link the real filename to the sampledocument.
+        bar.inc(1);
     }
+    bar.finish();
     Ok(())
 }
